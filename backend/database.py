63f754d205fa4467020818cb9dcfbc96cacdc7e6
Shubham Gupta <122731453+shubhamgupta0903@users.noreply.github.com>
2025-04-26 18:52:43 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-client = AsyncIOMotorClient("mongodb+srv://202251126:U2YB6N3fO7F7p4eV@cluster0.yx7y7.mongodb.net/")
+client = AsyncIOMotorClient("Your_Database_URL")
 db = client["5g"]
 users_collection = db["user_data"]
 chat_collection = db["chat_history"] 
